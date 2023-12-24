61707de7a82b1fac0257a6bcd9be85174a20a7a1
SHivaBharath2000 <152796403+SHivaBharath2000@users.noreply.github.com>
2023-12-24 11:04:06 +0530

    Add files via upload

---
+++ Xml,http/script.js
@@ -0,0 +1,10 @@
+function foo(){
+var first=document.getElementById("firstname").value;
+var middle=document.getElementById("middlename").value;
+var last=document.getElementById("lastname").value;
+var res=document.getElementById("email").value;
+console.log(first);
+console.log(middle);
+console.log(last);
+console.log(res);
+}
